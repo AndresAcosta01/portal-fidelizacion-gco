d5e82c1ccbb20cf68c6b890a861479ad22398aed
AndresAcosta01 <andresacost12a@gmail.com>
2026-09-05 08:48:11 -0500

    feat: agrega repositorio de ciudad
    - crea el contrato ICiudadRepositorio con JpaRepository
    - agrega consulta para obtener ciudades activas por departamento

---
+++ backend/src/main/java/com/gco/fidelizacion/repositories/ICiudadRepositorio.java
@@ -0,0 +1,15 @@
+package com.gco.fidelizacion.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gco.fidelizacion.models.Ciudad;
+
+@Repository
+public interface ICiudadRepositorio extends JpaRepository<Ciudad, UUID>{
+
+    List<Ciudad> findByDepartamento_IdAndActivoTrue(UUID id);
+}
