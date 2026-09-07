4999e03f5de97d403d7f476bdb975a7ba6ae8b5d
AndresAcosta01 <andresacost12a@gmail.com>
2026-09-06 21:07:38 -0500

    feat: diseña pagina principal de fidelizacion
    - crea experiencia visual orientada al cliente
    - incorpora seccion informativa del programa
    - agrega marcas con sus logos
    - añade llamados a registro
    - adapta la pagina a diseño responsive con Tailwind

--- frontend/src/app/features/inicio/inicio.ts
+++ frontend/src/app/features/inicio/inicio.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.html',
-  styleUrl: './inicio.css',
+  styleUrl: './inicio.css'
 })
-export class Inicio {}
+export class Inicio {
+
+  marcas = [
+    {
+      nombre: 'Americanino',
+      logo: 'https://res.cloudinary.com/lrur0zig/image/upload/v1788741251/Icono-americanino.png'
+    },
+    {
+      nombre: 'American Eagle',
+      logo: 'https://res.cloudinary.com/lrur0zig/image/upload/v1788740287/logo-american-eagle.svg'
+    },
+    {
+      nombre: 'Chevignon',
+      logo: 'https://res.cloudinary.com/lrur0zig/image/upload/v1788740368/Icono-chevignon.png'
+    },
+    {
+      nombre: 'Esprit',
+      logo: 'https://res.cloudinary.com/lrur0zig/image/upload/v1788740571/Icono-esprit.png'
+    },
+    {
+      nombre: 'Naf Naf',
+      logo: 'https://res.cloudinary.com/lrur0zig/image/upload/v1788740061/Icono-Naf-Naf.png'
+    },
+    {
+      nombre: 'Rifle',
+      logo: 'https://res.cloudinary.com/lrur0zig/image/upload/v1788741059/logo-rifle.png'
+    }
+  ];
+
+}
